5ca64d27643bcd05cc7873973c42147add6615cd
dishowrov <dinislamshowrov.2002@gmail.com>
2024-09-30 17:41:48 +0600

    removed faul pages

--- WPLMS child/assets/js/custom.js
+++ WPLMS child/assets/js/custom.js
@@ -1,6 +1,14 @@
-$('.dis-course-slider').slick({
+jQuery(function ($) {
+  $('.dis-course-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+  $('.elementor-tab-title').click(function () {
+    setTimeout(function () {
+      $('.dis-course-slider').slick('setPosition');
+    }, 0); 
+  });
+});
